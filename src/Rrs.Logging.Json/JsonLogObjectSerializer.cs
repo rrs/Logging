faf0a5343b89b8974394a977cc7d266ee01b8259
Robert Snipe <robertjsnipe@gmail.com>
2022-06-09 14:47:55 +0100

    added custom exception serialization to remove WatsonBuckets (which can be huge). Upped min version to framework version 4.6.2

--- src/Rrs.Logging.Json/JsonLogObjectSerializer.cs
+++ src/Rrs.Logging.Json/JsonLogObjectSerializer.cs
@@ -1,9 +1,21 @@
 ﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Rrs.Logging.Json
 {
     public class JsonLogObjectSerializer : ILogObjectSerializer
     {
-        public string Serialize(object o) => JsonConvert.SerializeObject(o);
+        private readonly bool _removeWatsonBuckets;
+        public JsonLogObjectSerializer(bool removeWatsonBuckets = true) => _removeWatsonBuckets = removeWatsonBuckets;
+
+        private static readonly JsonSerializerSettings _settings = new JsonSerializerSettings
+        {
+            Converters = new List<JsonConverter>
+            {
+                new ExceptionMinimalConverter()
+            },
+        };
+
+        public string Serialize(object o) => JsonConvert.SerializeObject(o, _removeWatsonBuckets ? _settings : null);
     }
 }
