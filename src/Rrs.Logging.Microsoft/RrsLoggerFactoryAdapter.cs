1ae3c67f5935afdb8fc88f8949b37db4435ed109
Rob Snipe <rob.snipe@pointone.co.uk>
2025-05-19 16:57:52 +0100

    added logger factory adapter

---
+++ src/Rrs.Logging.Microsoft/RrsLoggerFactoryAdapter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Rrs.Logging;
+
+internal class RrsLoggerFactoryAdapter : Microsoft.Extensions.Logging.ILoggerFactory
+{
+    private readonly ILogger _logger;
+
+    public RrsLoggerFactoryAdapter(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public void AddProvider(ILoggerProvider provider)
+    {
+    }
+
+    public Microsoft.Extensions.Logging.ILogger CreateLogger(string categoryName)
+    {
+        return new RrsLoggerAdapter(_logger);
+    }
+
+    public void Dispose()
+    {
+    }
+}
